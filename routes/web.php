f1d13e2b19978adf5d796cfc0621ac5a18451c7c
Seu nome para exibiçãoMartarelli <renanmartarelli@gmail.com>
2023-01-17 18:35:49 -0300

    alteração nome da controller

--- routes/web.php
+++ routes/web.php
@@ -50,7 +50,7 @@ Route::group(['middleware' => 'auth'], function () {
 
 //EXERCICIOS DOCUMENTOS
 Route::post('/photo', [DocumentsController::class, 'storePhoto']);
-Route::post('/images', [ImagesController::class, 'storeImage']);
+Route::post('/images', [DocumentsController::class, 'storeImage']);
 
 //Rotas de autenticação
 Route::get('/login', [UserController::class, 'login']) -> name('login');
